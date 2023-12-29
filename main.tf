306fb9c5a86e3f727e641f8691d932b6aa25ed35
bindu <sindhudesalanka@gmail.com>
2023-12-29 10:50:44 +0530

    a

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile = aws_iam_instance_profile.profile.name
 
   tags = {
     Name = "${var.component}.${var.env}"
